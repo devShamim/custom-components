e42651b0e7ec1141c273ccc26e51d700541c60e3
shamimsaj <shamimeforb@gmail.com>
2020-03-11 14:03:50 +0600

    tweaks js issue

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 /* custom dropdown */
 const atbdDropdown = document.querySelectorAll('.atbd-dropdown');
+// toggle dropdown
 atbdDropdown.forEach(function(el) {
         el.querySelector('.atbd-dropdown-toggle').addEventListener('click', function(e) {
                 e.preventDefault();
                 el.querySelector('.atbd-dropdown-items').classList.toggle('atbd-show');
         });
 });
+
+// remvoe toggle when click outside
 document.body.addEventListener('click', function(e) {
         if (e.target.getAttribute('data-drop-toggle') !== 'atbd-toggle') {
                 document.querySelectorAll('.atbd-dropdown-items').forEach(function(el) {
