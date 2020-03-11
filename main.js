2b179e7ba093aa1e7d7be8f452b7df61a762bb71
shamimsaj <shamimeforb@gmail.com>
2020-03-11 14:50:20 +0600

    tweaks js issue

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 /* custom dropdown */
 const atbdDropdown = document.querySelectorAll('.atbd-dropdown');
+
 // toggle dropdown
+let clickCount = 0;
 atbdDropdown.forEach(function(el) {
         el.querySelector('.atbd-dropdown-toggle').addEventListener('click', function(e) {
                 e.preventDefault();
-                document.querySelectorAll('.atbd-dropdown-items').forEach(function(elem) {
-                        elem.classList.remove('atbd-show');
-                });
-                el.querySelector('.atbd-dropdown-items').classList.toggle('atbd-show');
+                clickCount++;
+                if (clickCount % 2 === 1) {
+                        document.querySelectorAll('.atbd-dropdown-items').forEach(function(elem) {
+                                elem.classList.remove('atbd-show');
+                        });
+                        el.querySelector('.atbd-dropdown-items').classList.add('atbd-show');
+                } else {
+                        document.querySelectorAll('.atbd-dropdown-items').forEach(function(elem) {
+                                elem.classList.remove('atbd-show');
+                        });
+                }
         });
 });
 
 // remvoe toggle when click outside
 document.body.addEventListener('click', function(e) {
         if (e.target.getAttribute('data-drop-toggle') !== 'atbd-toggle') {
+                clickCount = 0;
                 document.querySelectorAll('.atbd-dropdown-items').forEach(function(el) {
                         el.classList.remove('atbd-show');
                 });
