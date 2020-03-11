1b3483ed3bb44f4c79a4ea25c6d5efbc5445b054
shamimsaj <shamimeforb@gmail.com>
2020-03-11 13:56:15 +0600

    tweaks js issue

--- main.js
+++ main.js
@@ -6,3 +6,10 @@ atbdDropdown.forEach(function(el) {
                 el.querySelector('.atbd-dropdown-items').classList.toggle('atbd-show');
         });
 });
+document.body.addEventListener('click', function(e) {
+        if (e.target.getAttribute('data-drop-toggle') !== 'atbd-toggle') {
+                document.querySelectorAll('.atbd-dropdown-items').forEach(function(el) {
+                        el.classList.remove('atbd-show');
+                });
+        }
+});
